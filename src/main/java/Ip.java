72c107c99d5ad77cb0c190b1baf79ae96292a009
Oleg <mrobenner@gmail.com>
2019-10-13 17:24:38 +0400

    Initial commit

---
+++ src/main/java/Ip.java
@@ -0,0 +1,13 @@
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+public class Ip extends UDF {
+    public Boolean evaluate(String one, String two) {
+        return matches(one, two);
+    }
+
+    private boolean matches(String ip, String subnet) {
+        IpAddressMatcher ipAddressMatcher = new IpAddressMatcher(subnet);
+        return ipAddressMatcher.matches(ip);
+    }
+}
